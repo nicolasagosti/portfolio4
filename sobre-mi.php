f7a711cba8dac38a865803498a9d6eeeb2518395
nicolasagosti <agostinicolas6557@gmail.com>
2021-05-24 22:45:27 -0300

    cambios

--- sobre-mi.php
+++ sobre-mi.php
@@ -27,16 +27,16 @@
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <ul class="navbar-nav me-auto mb-2 mb-md-0 mt-1">
                         <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="index.html">Inicio</a>
+                            <a class="nav-link px-4 py-1" href="index.php">Inicio</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active px-5 py-1" href="sobre-mi.html">Sobre mi</a>
+                            <a class="nav-link active px-5 py-1" href="sobre-mi.php">Sobre mi</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link  px.4 py-1" href="proyectos.html">Proyectos</a>
+                            <a class="nav-link  px.4 py-1" href="proyectos.php">Proyectos</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link px-4 py-1 " href="contactos.html">Contacto</a>
+                            <a class="nav-link px-4 py-1 " href="contactos.php">Contacto</a>
                         </li>
                     </ul>
                     <div class="d-inline">
